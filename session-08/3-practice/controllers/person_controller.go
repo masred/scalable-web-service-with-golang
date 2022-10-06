e798e22131a4610b5f4a7ba8e7417d3aeaf14524
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2022-10-07 02:26:03 +0700

    finish with bug

--- session-08/3-practice/controllers/person_controller.go
+++ session-08/3-practice/controllers/person_controller.go
@@ -29,3 +29,98 @@ func (idb *InDB) GetPerson(c *gin.Context) {
 
 	c.JSON(http.StatusOK, result)
 }
+
+func (idb *InDB) GetPersons(c *gin.Context) {
+	var (
+		persons []models.Person
+		result  gin.H
+	)
+
+	idb.DB.Find(&persons)
+	if len(persons) <= 0 {
+		result = gin.H{
+			"result": nil,
+			"count":  0,
+		}
+	} else {
+		result = gin.H{
+			"result": persons,
+			"count":  len(persons),
+		}
+	}
+
+	c.JSON(http.StatusOK, result)
+}
+
+func (idb *InDB) CreatePerson(c *gin.Context) {
+	var (
+		person models.Person
+		result gin.H
+	)
+	first_name := c.PostForm("first_name")
+	last_name := c.PostForm("last_name")
+	person.FirstName = first_name
+	person.LastName = last_name
+	idb.DB.Create(&person)
+	result = gin.H{
+		"result": person,
+	}
+	c.JSON(http.StatusOK, result)
+}
+
+func (idb *InDB) UpdatePerson(c *gin.Context) {
+	id := c.Query("id")
+	first_name := c.PostForm("first_name")
+	last_name := c.PostForm("last_name")
+	var (
+		person    models.Person
+		newPerson models.Person
+		result    gin.H
+	)
+
+	err := idb.DB.First(&person, id).Error
+	if err != nil {
+		result = gin.H{
+			"result": "data not found",
+		}
+	}
+	newPerson.FirstName = first_name
+	newPerson.LastName = last_name
+	err = idb.DB.Model(&person).Updates(newPerson).Error
+	if err != nil {
+		result = gin.H{
+			"result": "update failed",
+		}
+	} else {
+		result = gin.H{
+			"result": "successfully updated data",
+		}
+	}
+	c.JSON(http.StatusOK, result)
+}
+
+func (idb *InDB) DeletePerson(c *gin.Context) {
+	var (
+		person models.Person
+		result gin.H
+	)
+	id := c.Param("id")
+	err := idb.DB.First(&person, id).Error
+	if err != nil {
+		result = gin.H{
+			"result": "data not found",
+		}
+	}
+	err = idb.DB.Delete(&person).Error
+	if err != nil {
+		result = gin.H{
+			"result": "delete failed",
+		}
+	} else {
+		result = gin.H{
+			"result": "Data deleted successfully",
+		}
+	}
+
+	c.JSON(http.StatusOK, result)
+}
--- session-08/3-practice/main.go
+++ session-08/3-practice/main.go
@@ -1,1 +1,22 @@
-package practice
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/masred/scalable-web-service-with-golang/session-08/3-practice/config"
+	"github.com/masred/scalable-web-service-with-golang/session-08/3-practice/controllers"
+)
+
+func main() {
+	db := config.DBInit()
+	inDB := &controllers.InDB{DB: db}
+
+	router := gin.Default()
+
+	router.GET("/persons", inDB.GetPersons)
+	router.POST("/persons", inDB.CreatePerson)
+	router.GET("/persons/:id", inDB.GetPerson)
+	router.PUT("/persons/:id", inDB.UpdatePerson)
+	router.DELETE("/persons/:id", inDB.DeletePerson)
+	router.Run(":3000")
+}
