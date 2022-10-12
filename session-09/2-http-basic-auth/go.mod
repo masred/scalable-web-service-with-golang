756a09902da9a404eee4fe8407d9218e5c0ca4b5
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2022-10-12 09:32:07 +0700

    feat: session 09 - basic auth

---
+++ session-09/2-http-basic-auth/go.mod
@@ -0,0 +1,3 @@
+module github.com/masred/scalable-web-service-with-golang/session-09/2-basic-auth
+
+go 1.19
---
+++ session-09/2-http-basic-auth/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Student struct {
+	Id    string
+	Name  string
+	Grade int32
+}
+
+var students = []*Student{}
+
+const USERNAME = "batman"
+const PASSWORD = "secret"
+
+func Auth(w http.ResponseWriter, r *http.Request) bool {
+	username, password, ok := r.BasicAuth()
+	if !ok {
+		w.Write([]byte(`something went wrong`))
+		return false
+	}
+
+	isValid := (username == USERNAME) && (password == PASSWORD)
+	if !isValid {
+		w.Write([]byte(`wrong username/password`))
+		return false
+	}
+
+	return true
+}
+
+func AllowOnlyGET(w http.ResponseWriter, r *http.Request) bool {
+	if r.Method != "GET" {
+		w.Write([]byte("Only GET is allowed"))
+		return false
+	}
+	return true
+}
+
+func GetStudents() []*Student {
+	return students
+}
+
+func SelectStudent(id string) *Student {
+	for _, each := range students {
+		if each.Id == id {
+			return each
+		}
+	}
+
+	return nil
+}
+
+func init() {
+	students = append(students, &Student{Id: "s001", Name: "Mas", Grade: 2})
+	students = append(students, &Student{Id: "s002", Name: "Red", Grade: 2})
+	students = append(students, &Student{Id: "s003", Name: "Masred", Grade: 3})
+}
+
+func main() {
+	http.HandleFunc("/student", ActionStudent)
+
+	server := new(http.Server)
+	server.Addr = ":9000"
+
+	fmt.Println("server started at localhost:9000")
+	server.ListenAndServe()
+}
+
+func ActionStudent(w http.ResponseWriter, r *http.Request) {
+	if !Auth(w, r) {
+		return
+	}
+	if !AllowOnlyGET(w, r) {
+		return
+	}
+	if id := r.URL.Query().Get("id"); id != "" {
+		OutputJSON(w, SelectStudent(id))
+		return
+	}
+
+	OutputJSON(w, GetStudents())
+}
+
+func OutputJSON(w http.ResponseWriter, o interface{}) {
+	res, err := json.Marshal(o)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Header().Set("Content-Type", "applicaion/json")
+	w.Write(res)
+}
