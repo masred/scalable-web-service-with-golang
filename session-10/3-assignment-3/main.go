10378d5fd59a993c532fe15beaac5ead5b46eb21
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2022-10-14 10:13:10 +0700

    feat: create html page with up to date data

--- session-10/3-assignment-3/main.go
+++ session-10/3-assignment-3/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"html/template"
 	"math/rand"
 	"os"
 	"time"
@@ -9,7 +10,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type JSON struct {
+type Json struct {
 	Status Status `json:"status"`
 }
 
@@ -19,21 +20,60 @@ type Status struct {
 }
 
 func main() {
-	var data JSON
+	var data Json
 
-	for range time.Tick(time.Second * 15) {
-		rand.Seed(time.Now().UnixNano())
+	go func() {
+		for range time.Tick(time.Second * 15) {
+			rand.Seed(time.Now().UnixNano())
 
-		data.Status.Water = rand.Intn(100)
-		data.Status.Wind = rand.Intn(100)
+			data.Status.Water = rand.Intn(100)
+			data.Status.Wind = rand.Intn(100)
 
-		file, _ := os.Create("status.json")
-		jsonData, _ := json.Marshal(data)
+			file, _ := os.Create("status.json")
+			jsonData, _ := json.Marshal(data)
 
-		file.WriteString(string(jsonData))
-		file.Close()
-	}
+			file.WriteString(string(jsonData))
+			file.Close()
+		}
+	}()
 
-	app := gin.Default()
-	app.Run()
+	router := gin.Default()
+	router.GET("/", func(ctx *gin.Context) {
+		t, err := template.ParseFiles("index.html")
+		if err != nil {
+			panic(err)
+		}
+
+		var data Json
+		dataFile, _ := os.ReadFile("status.json")
+		json.Unmarshal(dataFile, &data)
+
+		var waterStatus string
+		if data.Status.Water > 8 {
+			waterStatus = "bahaya"
+		} else if data.Status.Water >= 6 {
+			waterStatus = "siaga"
+		} else {
+			waterStatus = "aman"
+		}
+
+		var windStatus string
+		if data.Status.Wind > 15 {
+			windStatus = "bahaya"
+		} else if data.Status.Water >= 7 {
+			windStatus = "siaga"
+		} else {
+			windStatus = "aman"
+		}
+
+		dataMap := map[string]interface{}{
+			"Water":       data.Status.Water,
+			"WaterStatus": waterStatus,
+			"Wind":        data.Status.Wind,
+			"WindStatus":  windStatus,
+		}
+
+		t.Execute(ctx.Writer, dataMap)
+	})
+	router.Run()
 }
