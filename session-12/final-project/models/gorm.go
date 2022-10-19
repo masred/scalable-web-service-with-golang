aceb63d6b15bd63f05ecb25060f5a21a8d9df1fe
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2022-10-19 20:48:23 +0700

    feat: add common field

---
+++ session-12/final-project/models/gorm.go
@@ -0,0 +1,9 @@
+package models
+
+import "time"
+
+type Gorm struct {
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
