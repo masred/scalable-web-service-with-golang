6fdfe60206601a403bc6bf43770e7a7e05741bb8
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2022-10-19 20:55:24 +0700

    feat: add omitempty tag in Gorm struct

--- session-12/final-project/models/gorm.go
+++ session-12/final-project/models/gorm.go
@@ -4,6 +4,6 @@ import "time"
 
 type Gorm struct {
 	ID        uint      `gorm:"primaryKey" json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	CreatedAt time.Time `json:"created_at,omitempty"`
+	UpdatedAt time.Time `json:"updated_at,omitempty"`
 }
