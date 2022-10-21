701a96fb0999c452290e5fe8fd84c8e839e5ffa6
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2022-10-21 22:56:46 +0700

    feat: add jwt generate and verification token

---
+++ session-12/final-project/helpers/jwt.go
@@ -0,0 +1,59 @@
+package helpers
+
+import (
+	"errors"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func GenerateToken(id string, email string) string {
+	claims := jwt.MapClaims{
+		"id":    id,
+		"email": email,
+	}
+
+	parseToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	signedToken, _ := parseToken.SignedString([]byte(os.Getenv("KEY")))
+
+	return signedToken
+}
+
+func VerifyToken(ctx *gin.Context) (interface{}, error) {
+	errResponse := errors.New("sign in to proceed")
+	headerToken := ctx.Request.Header.Get("Authorization")
+	bearer := strings.HasPrefix(headerToken, "Bearer")
+
+	if !bearer {
+		return nil, errResponse
+	}
+
+	stringToken := strings.Split(headerToken, " ")[1]
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file: ", err)
+	}
+
+	token, _ := jwt.Parse(stringToken, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errResponse
+		}
+
+		return []byte(os.Getenv("KEY")), nil
+	})
+
+	if _, ok := token.Claims.(jwt.MapClaims); !ok && !token.Valid {
+		return nil, errResponse
+	}
+
+	return token.Claims.(jwt.MapClaims), nil
+}
