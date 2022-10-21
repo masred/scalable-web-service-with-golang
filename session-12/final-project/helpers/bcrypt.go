2680b004d7cff53715296c281b3cda85ad4f6a87
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2022-10-21 22:56:12 +0700

    feat: add helper for hash and compare password

---
+++ session-12/final-project/helpers/bcrypt.go
@@ -0,0 +1,18 @@
+package helpers
+
+import "golang.org/x/crypto/bcrypt"
+
+func Hash(password string) string {
+	salt := 8
+	pass := []byte(password)
+	hash, _ := bcrypt.GenerateFromPassword(pass, salt)
+
+	return string(hash)
+}
+
+func Compare(hashedPassword, password []byte) bool {
+	hash, pass := []byte(hashedPassword), []byte(password)
+	err := bcrypt.CompareHashAndPassword(hash, pass)
+
+	return err == nil
+}
