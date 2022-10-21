db22340bf3e50afea258d89fe19012a1c2856112
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2022-10-21 17:06:34 +0700

    feat: connect to database

---
+++ session-12/final-project/app/database.go
@@ -0,0 +1,37 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/masred/scalable-web-service-with-golang/session-12/final-project/models/domain"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewDB() *gorm.DB {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	host := os.Getenv("PGHOST")
+	user := os.Getenv("PGUSER")
+	password := os.Getenv("PGPASSWORD")
+	dbname := os.Getenv("PGDBNAME")
+	port := os.Getenv("PGPORT")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := db.AutoMigrate(&domain.User{}, &domain.Photo{}, &domain.Comment{}, &domain.SocialMedia{}); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	return db
+}
