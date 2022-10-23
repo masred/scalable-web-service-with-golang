2b78c567e1a1a034f61d82cbc0a64e0bcdb02b7b
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2022-10-23 20:20:06 +0700

    fix: delete gorm.go file

--- session-12/final-project/model/gorm.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "time"
-
-type Gorm struct {
-	ID        string    `gorm:"primaryKey;type:VARCHAR(100)" json:"id"`
-	CreatedAt time.Time `json:"created_at,omitempty"`
-	UpdatedAt time.Time `json:"updated_at,omitempty"`
-}
